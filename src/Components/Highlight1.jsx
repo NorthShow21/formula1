47cb00226b6f27981a0a01c376761cf84153f80c
Joel <vynandrog@gmail.com>
2025-11-25 10:42:08 +0700

    4th commit

--- src/Components/Highlight1.jsx
+++ src/Components/Highlight1.jsx
@@ -1,9 +1,15 @@
 function Highlight1(){
     return(
         <section className="highlight1">
-            <div className="highlight-country"></div>
-            <div className="highlight-circuit-name"></div>
-            <div className="highlight-date"></div>
+            <div className="highlight-country">
+
+            </div>
+            <div className="highlight-circuit-name">
+
+            </div>
+            <div className="highlight-date">
+                
+            </div>
         </section>
     );
 }
