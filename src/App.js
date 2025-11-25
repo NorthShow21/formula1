2af5a17fd7b6923e5abc140753093705b1621c1c
Joel <vynandrog@gmail.com>
2025-11-25 10:41:47 +0700

    4th commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 
 import './App.css';
 import Header from './Components/Header';
+import Highlight1 from './Components/Highlight1';
 
 function App() {
   return (
-    <header className='header'>
-      <Header />
-    </header>
+    <>
     
+      <header className='header'>
+        <Header />
+      </header>
+      
+      {/* Buat Highlight */}
+      <section className='highlight'>
+        <h1>HIGHLIGHTS</h1>
+        <Highlight1 />
+      </section>
+    
+
+
+    </>
   );
 }
 
---
+++ src/Components/Highlight1.jsx
@@ -0,0 +1,11 @@
+function Highlight1(){
+    return(
+        <section className="highlight1">
+            <div className="highlight-country"></div>
+            <div className="highlight-circuit-name"></div>
+            <div className="highlight-date"></div>
+        </section>
+    );
+}
+
+export default Highlight1;
